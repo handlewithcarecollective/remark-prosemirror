b776b6c7691fc8d173ccc2833e32534974e3fde7
Shane Friedman <smoores-gpg@friedmans.us>
2024-12-18 12:43:10 -0500

    Resolve cast through unknown

--- lib/remark-prosemirror.ts
+++ lib/remark-prosemirror.ts
@@ -1,20 +1,15 @@
 import { type Root as MdastRoot } from "mdast";
 import { type Node as PmNode } from "prosemirror-model";
-import { type Plugin, type Processor } from "unified";
+import { type Plugin } from "unified";
 import { Options, toProseMirror } from "./mdast-util-to-prosemirror.js";
 
 export { toPmNode, toPmMark } from "./mdast-util-to-prosemirror.js";
 
 export type { Options };
 
-export const remarkProseMirror = function (
-  this: Processor<undefined, undefined, undefined, MdastRoot, PmNode>,
-  options: Options,
-) {
-  this.compiler = function (tree) {
-    return toProseMirror(tree, options);
+export const remarkProseMirror: Plugin<[Options], MdastRoot, PmNode> =
+  function (options: Options) {
+    this.compiler = function (tree) {
+      return toProseMirror(tree as MdastRoot, options);
+    };
   };
-  // We have to do this ugly cast through unknown here because the
-  // Plugin type just uses `Processor` without any type params, which
-  // makes it incompatible with our more correctly typed Processor<..., MdastRoot, string>
-} as unknown as Plugin<[Options], MdastRoot, PmNode>;
